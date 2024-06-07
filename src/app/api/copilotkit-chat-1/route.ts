a7377dd1df21cff73651e48e99a33e07c3f6bab2
Fabio Orlinski <fabio.orlinski@gmail.com>
2024-06-07 10:54:31 -0300

    Create smartGoalValidationFunction

--- src/app/api/copilotkit-chat-1/route.ts
+++ src/app/api/copilotkit-chat-1/route.ts
@@ -2,9 +2,6 @@
 import { CopilotRuntime, OpenAIAdapter } from "@copilotkit/backend";
 import { smartGoalValidationFunction } from "./smartGoalValidation";
 import { AnnotatedFunction } from "@copilotkit/shared";
-// import assessmentsData from '../../../../data/assessments.json';
-import playsData from "../../../../data/plays.json";
-import questions from "../../../../data/assessments.json";
 
 export interface Answer {
   question: string;
@@ -14,29 +11,6 @@ export interface Answer {
 // Define a runtime environment variable, indicating the environment where the code is expected to run.
 export const runtime = "edge";
 
-// Define an annotated function for the chatbot. This object includes metadata and an implementation for the function.
-const generatePlays: AnnotatedFunction<any> = {
-  name: "generatePlays", // Function name.
-  description: "Call this function to generate the plays for the user", // Function description.
-  argumentAnnotations: [
-    // Annotations for arguments that the function accepts.
-    {
-      name: "questionAndAnswers", // Argument name.
-      type: "string", // Argument type.
-      description:
-        "The questions that were asked to the user with their corresponding response. With the format, question: answer", // Argument description.
-      required: true, // Indicates that the argument is required.
-    },
-  ],
-  implementation: async (answers) => {
-    // The actual function implementation.
-    const existingPlays = JSON.stringify(playsData);
-    console.log("answers =>", answers);
-    const result = await createChatbotFlow(answers, existingPlays);
-    console.log("result =>", result);
-    return result;
-  },
-};
 
 const isSmartGoal: AnnotatedFunction<any> = {
   name: "isSmartGoal", // Function name.
@@ -82,9 +56,7 @@ const isSmartGoal: AnnotatedFunction<any> = {
 // Define an asynchronous function that handles POST requests.
 export async function POST(req: Request): Promise<Response> {
   const actions: AnnotatedFunction<any>[] = []; // Initialize an array to hold actions.
-  if (process.env["TAVILY_API_KEY"]) {
-    actions.push(isSmartGoal);
-  }
+  actions.push(isSmartGoal);
   const copilotKit = new CopilotRuntime({
     actions: actions,
   });
